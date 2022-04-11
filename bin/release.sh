3ecce7f275f423e5d8b9234425f0c2b6205fe16b
Joe Haines <hello@joehaines.co.uk>
2022-04-11 16:46:34 +0100

    Update release script to work for bugsnag-expo

--- bin/release.sh
+++ bin/release.sh
@@ -12,33 +12,15 @@ if [[ -z ${GITHUB_USER:-} ]]; then error_missing_field "GITHUB_USER"; fi
 if [[ -z ${GITHUB_ACCESS_TOKEN:-} ]]; then error_missing_field "GITHUB_ACCESS_TOKEN"; fi
 if [[ -z ${RELEASE_BRANCH:-} ]]; then error_missing_field "RELEASE_BRANCH"; fi
 if [[ -z ${VERSION:-} ]]; then error_missing_field "VERSION"; fi
-if [[ -z ${AWS_ACCESS_KEY_ID:-} ]]; then error_missing_field "AWS_ACCESS_KEY_ID"; fi
-if [[ -z ${AWS_SECRET_ACCESS_KEY:-} ]]; then error_missing_field "AWS_SECRET_ACCESS_KEY"; fi
 
 git clone --single-branch \
   --branch "$RELEASE_BRANCH" \
-  https://"$GITHUB_USER":"$GITHUB_ACCESS_TOKEN"@github.com/bugsnag/bugsnag-js.git
+  https://"$GITHUB_USER":"$GITHUB_ACCESS_TOKEN"@github.com/bugsnag/bugsnag-expo.git
 
-cd /app/bugsnag-js
+cd /app/bugsnag-expo
 
-# "ci" rather than "install" ensures the process doesn't make the work tree dirty by modifying lockfiles
-npm ci
-npm run bootstrap -- --ci
-
-npx lerna run build \
-  --scope @bugsnag/node \
-  --scope @bugsnag/browser \
-  --scope @bugsnag/expo
-
-npx lerna run build \
-  --ignore @bugsnag/node\
-  --ignore @bugsnag/browser \
-  --ignore @bugsnag/expo \
-  --ignore @bugsnag/plugin-electron-app \
-  --ignore @bugsnag/plugin-electron-client-state-persistence
-
-# check if the browser package changed – if it didn't we don't need to upload to the CDN
-BROWSER_PACKAGE_CHANGED=$(npx lerna changed --parseable | grep -c packages/js$ || test $? = 1;)
+npm install --package-lock false
+npx lerna bootstrap -- --package-lock false
 
 if [ -v RETRY_PUBLISH ]; then
   npx lerna publish from-package
@@ -52,8 +34,4 @@ else
       npx lerna publish "$VERSION"
       ;;
   esac
-fi
-
-if [ "$BROWSER_PACKAGE_CHANGED" -eq 1 ] || [  -v FORCE_CDN_UPLOAD ]; then
-  npm run cdn-upload
 fi
