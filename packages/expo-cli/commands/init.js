59933965b7acdea1ede79aedb35be151b1698e18
Ben Wilson <ben.wilson@smartbear.com>
2022-07-19 16:03:47 +0100

    detect eas build before installing plugin

--- packages/expo-cli/commands/init.js
+++ packages/expo-cli/commands/init.js
@@ -1,11 +1,22 @@
 const install = require('./install')
 const setApiKey = require('./set-api-key')
 const insert = require('./insert')
+const addHook = require('./add-hook')
 const uploadSourcemaps = require('./upload-sourcemaps')
+const { access } = require('fs')
+const { join } = require('path')
 
 module.exports = async (argv, globalOpts) => {
   await install(argv, globalOpts)
   await setApiKey(argv, globalOpts)
   await insert(argv, globalOpts)
-  await uploadSourcemaps(argv, globalOpts)
+
+  const projectRoot = globalOpts['project-root']
+  const easPresent = await access(join(projectRoot, 'eas.json'))
+
+  if (easPresent) {
+    await uploadSourcemaps(argv, globalOpts)
+  } else {
+    await addHook(argv, globalOpts)
+  }
 }
