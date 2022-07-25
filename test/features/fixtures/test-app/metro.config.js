e2ebe4a857e9b9a90808b0fc5eb58cff3685f15c
Ben Wilson <ben.wilson@smartbear.com>
2022-07-25 12:01:54 +0100

    update test fixture to work with eas builds

--- test/features/fixtures/test-app/metro.config.js
+++ test/features/fixtures/test-app/metro.config.js
@@ -1,15 +1,15 @@
-const { join } = require('path')
+const { getDefaultConfig } = require('@expo/metro-config');
 
-module.exports = {
-  resolver: {
-    extraNodeModules: {
-      'expo': `${__dirname}/node_modules/expo`,
-      'expo-modules-core': `${__dirname}/node_modules/expo-modules-core`,
-      'react-native': `${__dirname}/node_modules/react-native`,
-      'react': `${__dirname}/node_modules/react`,
-      '@babel/runtime': `${__dirname}/node_modules/@babel/runtime`,
-      'promise': `${__dirname}/node_modules/promise`,
-      '@unimodules/core': `${__dirname}/node_modules/@unimodules/core`,
-    }
-  }
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.extraNodeModules = {
+  'expo': `${__dirname}/node_modules/expo`,
+  'expo-modules-core': `${__dirname}/node_modules/expo-modules-core`,
+  'react-native': `${__dirname}/node_modules/react-native`,
+  'react': `${__dirname}/node_modules/react`,
+  '@babel/runtime': `${__dirname}/node_modules/@babel/runtime`,
+  'promise': `${__dirname}/node_modules/promise`,
+  '@unimodules/core': `${__dirname}/node_modules/@unimodules/core`,
 }
+
+module.exports = defaultConfig;
