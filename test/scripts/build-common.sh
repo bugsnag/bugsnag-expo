0e848f960e3700ba94f511e8239669e1bd03bd54
Alex Moinet <alex@bugsnag.com>
2022-08-04 15:36:51 +0100

    Add extra build steps to common script

---
+++ test/scripts/build-common.sh
@@ -0,0 +1,22 @@
+# Lets make sure the build folder was cleared out correctly
+rm -rf $BUILDKITE_BUILD_CHECKOUT_PATH/build/*
+
+# Install expo requirements
+npm Install
+
+# Pack the packages and move them to the test fixture
+npm pack packages/*/
+mv *.tgz test/features/fixtures/test-app
+
+cd test/features/fixtures/test-app
+
+# Install the bugsnag-expo-cli and run setup
+npm install bugsnag-expo-cli*.tgz && rm bugsnag-expo-cli*.tgz && ./run-bugsnag-expo-cli
+
+# set bugsnag-js override versions if this build was triggered from the bugsnag-js repo
+./set-bugsnag-js-overrides $BUGSNAG_JS_BRANCH $BUGSNAG_JS_COMMIT
+
+# install the remaining packages, this also re-installs the correct @bugsnag/expo version
+npm install *.tgz && rm *.tgz
+
+echo $EXPO_CREDENTIALS_BASE64 | base64 -d > credentials.json
--- test/scripts/build-ios.sh
+++ test/scripts/build-ios.sh
@@ -2,16 +2,9 @@
 
 set -e
 
-# Lets make sure the build folder was cleared out correctly
-rm -rf $BUILDKITE_BUILD_CHECKOUT_PATH/build/*
-
-cd test/features/fixtures/test-app
-
-echo $EXPO_CREDENTIALS_BASE64 | base64 -d > credentials.json
+./test/scripts/build-common.sh
 
 mkdir build
-
-npm install
 
 eas build \
   --local \
