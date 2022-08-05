150e0e39331d8d1419de154475e63887db4f2b3e
Alex Moinet <alex@bugsnag.com>
2022-08-05 12:28:37 +0100

    Move install command

--- test/scripts/build-common.sh
+++ test/scripts/build-common.sh
@@ -19,6 +19,14 @@ npm install bugsnag-expo-cli*.tgz && ./run-bugsnag-expo-cli
 # install the remaining packages, this also re-installs the correct @bugsnag/expo version
 npm install *.tgz
 
+# install
+spawn npx bugsnag-expo-cli install
+
+expect "@bugsnag/expo does not appear to be installed, do you want to install it and its dependencies??"
+send "y\r"
+
+expect eof
+
 # As EAS uses yarn, pre-run the install for the sake of my sanity
 yarn install
 
