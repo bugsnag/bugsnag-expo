f0c30bd6bd91d3b4727012da7495ecd60a733f41
Alex Moinet <alex@bugsnag.com>
2022-08-10 16:10:14 +0100

    Use dyanmic implementation of config again

--- test/features/fixtures/test-app/app.config.js
+++ test/features/fixtures/test-app/app.config.js
@@ -1,10 +1,10 @@
 import withRemoveiOSNotificationEntitlement from "./config-plugins/withRemoveiOSNotificationEntitlement";
 
 export default (config) => {
+    console.log(config)
     return {
         ...config,
         plugins: [
-            ...(config.plugins ?? []),
             [withRemoveiOSNotificationEntitlement]
         ]
     }
