67384405a44d2399cb6a9114216463f14f435205
Alex Moinet <alex@bugsnag.com>
2022-08-10 17:27:16 +0100

    Different syntax but still dynamic

--- test/features/fixtures/test-app/app.config.js
+++ test/features/fixtures/test-app/app.config.js
@@ -1,13 +1,10 @@
 import withRemoveiOSNotificationEntitlement from "./config-plugins/withRemoveiOSNotificationEntitlement";
 
-export default (config) => {
-    console.log(config)
-    var newConfig = {
+export default config => {
+    return {
         ...config,
         plugins: [
             [withRemoveiOSNotificationEntitlement]
         ]
     }
-    console.log(newConfig)
-    return newConfig
 }
