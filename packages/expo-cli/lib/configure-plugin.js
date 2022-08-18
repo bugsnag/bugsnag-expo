6eccbf91d0cdb3b86df84a8fd1331aa1147b3d1b
Ben Wilson <ben.wilson@smartbear.com>
2022-08-18 18:01:26 +0100

    add workspaces detection for plugin configuration

--- packages/expo-cli/lib/configure-plugin.js
+++ packages/expo-cli/lib/configure-plugin.js
@@ -1,8 +1,31 @@
 const { join } = require('path')
 const { readFile, writeFile } = require('fs')
 const { promisify } = require('util')
+const { spawn } = require('child_process')
+const { blue } = require('kleur')
 
 const plugin = '@bugsnag/plugin-expo-eas-sourcemaps'
+
+function usingWorkspaces (projectRoot) {
+  return new Promise((resolve, reject) => {
+    const command = ['workspaces', 'info']
+    const proc = spawn('yarn', command, { cwd: projectRoot })
+
+    // buffer output in case of an error
+    proc.stdout.on('data', d => {})
+    proc.stderr.on('data', d => {})
+
+    proc.on('error', err => { reject(err) })
+
+    proc.on('close', code => {
+      if (code === 0) {
+        resolve(true)
+      }
+
+      resolve(false)
+    })
+  })
+}
 
 module.exports = async (projectRoot) => {
   try {
@@ -25,27 +48,34 @@ module.exports = async (projectRoot) => {
     throw e
   }
 
-  try {
-    const sourceMaps = '@bugsnag/source-maps'
-    const packageJsonPath = join(projectRoot, 'package.json')
-    const packageJson = JSON.parse(await promisify(readFile)(packageJsonPath))
-    packageJson.workspaces = packageJson.workspaces || {}
-    packageJson.workspaces.nohoist = packageJson.workspaces.nohoist || []
-    if (!packageJson.workspaces.nohoist.includes(plugin)) {
-      packageJson.workspaces.nohoist.push(plugin)
+  // are we in a monorepo?
+  const addMonorepoConfig = await usingWorkspaces(projectRoot)
+
+  if (addMonorepoConfig) {
+    console.log(blue('> yarn workspaces detected, updating config'))
+
+    try {
+      const sourceMaps = '@bugsnag/source-maps'
+      const packageJsonPath = join(projectRoot, 'package.json')
+      const packageJson = JSON.parse(await promisify(readFile)(packageJsonPath))
+      packageJson.workspaces = packageJson.workspaces || {}
+      packageJson.workspaces.nohoist = packageJson.workspaces.nohoist || []
+      if (!packageJson.workspaces.nohoist.includes(plugin)) {
+        packageJson.workspaces.nohoist.push(plugin)
+      }
+      if (!packageJson.workspaces.nohoist.includes(sourceMaps)) {
+        packageJson.workspaces.nohoist.push(sourceMaps)
+      }
+      await promisify(writeFile)(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8')
+    } catch (e) {
+      // swallow and rethrow for errors that we can produce better messaging
+      if (e.code === 'ENOENT') {
+        throw new Error(`Couldn’t find package.json in "${projectRoot}".`)
+      }
+      if (e.name === 'SyntaxError') {
+        throw new Error(`Couldn’t parse package.json because it wasn’t valid JSON: "${e.message}"`)
+      }
+      throw e
     }
-    if (!packageJson.workspaces.nohoist.includes(sourceMaps)) {
-      packageJson.workspaces.nohoist.push(sourceMaps)
-    }
-    await promisify(writeFile)(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8')
-  } catch (e) {
-    // swallow and rethrow for errors that we can produce better messaging
-    if (e.code === 'ENOENT') {
-      throw new Error(`Couldn’t find package.json in "${projectRoot}".`)
-    }
-    if (e.name === 'SyntaxError') {
-      throw new Error(`Couldn’t parse package.json because it wasn’t valid JSON: "${e.message}"`)
-    }
-    throw e
   }
 }
