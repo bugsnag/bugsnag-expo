df10ec37b3e45b961adff3c7c9d70bc933fa6abf
Alex Moinet <alex@bugsnag.com>
2022-08-23 15:59:28 +0100

    Use yarn and explicitly add required packages

--- test/scripts/build-common.sh
+++ test/scripts/build-common.sh
@@ -13,18 +13,23 @@ mv *.tgz test/features/fixtures/test-app
 cd test/features/fixtures/test-app
 
 # Install the bugsnag-expo-cli and run setup
-npm install bugsnag-expo-cli*.tgz && ./run-bugsnag-expo-cli
+BUGSNAG_EXPO_CLI=$(ls | grep -e bugsnag-expo-cli*.tgz)
+yarn add file:$BUGSNAG_EXPO_CLI
+./run-bugsnag-expo-cli
 
 # set bugsnag-js override versions if this build was triggered from the bugsnag-js repo
 ./set-bugsnag-js-overrides $BUGSNAG_JS_BRANCH $BUGSNAG_JS_COMMIT
 
 # install the remaining packages, this also re-installs the correct @bugsnag/expo version
-npm install *.tgz
+for FILE in *.tgz; do
+    yarn add file:$FILE
+done
 
-./run-bugsnag-expo-cli-install
 
 # As EAS uses yarn, pre-run the install for the sake of my sanity
 yarn install
+
+./run-bugsnag-expo-cli-install
 
 cp $EXPO_CREDENTIALS_PATH credentials.json
 
