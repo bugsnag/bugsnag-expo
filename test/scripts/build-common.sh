612800c07deeba6184aedc65a89b3835acdcdbf4
Alex Moinet <alex@bugsnag.com>
2022-08-31 21:01:58 +0100

    Don't use the yarn import

--- test/scripts/build-common.sh
+++ test/scripts/build-common.sh
@@ -26,7 +26,6 @@ npm install bugsnag-expo-cli*.tgz
 
 # install the remaining packages, this also re-installs the correct @bugsnag/expo version
 npm install *.tgz
-yarn import
 
 ./run-bugsnag-expo-cli-install
 
