5e221acc7e4e34faecba419aca4d076c45edad34
Ben Wilson <ben.wilson@smartbear.com>
2022-09-12 14:44:05 +0100

    add hoisting limits to plugin configuration for v2

--- packages/expo-cli/lib/configure-plugin.js
+++ packages/expo-cli/lib/configure-plugin.js
@@ -73,6 +73,11 @@ module.exports = async (projectRoot) => {
       if (!packageJson.workspaces.nohoist.includes(sourceMaps)) {
         packageJson.workspaces.nohoist.push(sourceMaps)
       }
+      if (withYarnV2) {
+        packageJson.installConfig = packageJson.installConfig || {}
+        packageJson.installConfig.hoistingLimits = 'workspaces'
+      }
+
       await promisify(writeFile)(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8')
     } catch (e) {
       // swallow and rethrow for errors that we can produce better messaging
