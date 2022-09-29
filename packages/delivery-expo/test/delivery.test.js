3be1dc1600f821fa2a66039471294cea543ee74f
Dan Skinner <dan@bugsnag.com>
2022-09-29 16:57:05 +0100

    explicitly mark failed payloads >1MB as not retryable

--- packages/delivery-expo/test/delivery.test.js
+++ packages/delivery-expo/test/delivery.test.js
@@ -202,6 +202,40 @@ describe('delivery: expo', () => {
     })
   })
 
+  it('does not attempt to re-send oversized payloads', done => {
+    // A 401 is considered retryable but this will be override by the payload size check
+    const { requests, server } = mockServer(401)
+    server.listen(err => {
+      expect(err).toBeUndefined()
+
+      const lotsOfEvents = []
+      while (JSON.stringify(lotsOfEvents).length < 10e5) {
+        lotsOfEvents.push({ errors: [{ errorClass: 'Error', errorMessage: 'long repetitive string'.repeat(1000) }] })
+      }
+      const payload = {
+        events: lotsOfEvents
+      }
+
+      const config = {
+        apiKey: 'aaaaaaaa',
+        endpoints: { notify: `http://0.0.0.0:${server.address().port}/notify/` },
+        redactedKeys: []
+      }
+
+      let didLog = false
+      const log = () => { didLog = true }
+
+      delivery({ _config: config, _logger: { error: log, info: () => {} } }, fetch).sendEvent(payload, (err) => {
+        expect(didLog).toBe(true)
+        expect(enqueueSpy).not.toHaveBeenCalled()
+        expect(err).toBeTruthy()
+        expect(requests.length).toBe(1)
+        server.close()
+        done()
+      })
+    })
+  })
+
   it('handles errors gracefully for sessions (ECONNREFUSED)', done => {
     const payload = {
       events: [{ errors: [{ errorClass: 'Error', errorMessage: 'foo is not a function' }] }]
