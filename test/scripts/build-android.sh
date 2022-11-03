b465cdacebf1e87a794fc0f754946bfb16f6ea66
Josh Edney <josh.edney@smartbear.com>
2022-11-03 22:02:40 +0000

    update build script and pipeline to switch node version

--- test/scripts/build-android.sh
+++ test/scripts/build-android.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Switch Node Version; Default to Node 14
+[ "${IN_SUBSHELL}" != "$0" ] && exec env IN_SUBSHELL="$0" nave use "${NODE_VERSION:-14}" bash "$0" "$@" || :
+
 ./test/scripts/build-common.sh
 
 cd test/features/fixtures/test-app
