2fc5fcc42323912f446c1fe1965347dcd35c3222
Josh Edney <josh.edney@smartbear.com>
2022-11-09 14:36:53 +0000

    update varibale used in sed for porject id

--- test/scripts/build-common.sh
+++ test/scripts/build-common.sh
@@ -21,6 +21,9 @@ cd test/features/fixtures/test-app
 npm install bugsnag-expo-cli*.tgz
 ./run-bugsnag-expo-cli
 
+# Set EAS Project ID
+sed -i '' "s/EXPO_EAS_PROJECT_ID/$EXPO_EAS_PROJECT_ID/g" app.json
+
 # set bugsnag-js override versions if this build was triggered from the bugsnag-js repo
 ./set-bugsnag-js-overrides $BUGSNAG_JS_BRANCH $BUGSNAG_JS_COMMIT
 
