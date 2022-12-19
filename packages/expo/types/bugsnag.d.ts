fc1760806bae8035dc8288582e41ba7615a0fa0b
Yousif Ahmed <yousif@bugsnag.com>
2022-12-19 16:11:51 +0000

    Use interface instead of intersection type

--- packages/expo/types/bugsnag.d.ts
+++ packages/expo/types/bugsnag.d.ts
@@ -1,6 +1,9 @@
 import { BugsnagStatic, Config, Client } from '@bugsnag/core'
 
-type ExpoConfig = Partial<Config> & { codeBundleId?: string }
+interface ExpoConfig extends Partial<Config> { 
+  codeBundleId?: string 
+}
+
 interface ExpoBugsnagStatic extends BugsnagStatic {
   start(apiKeyOrOpts?: string | ExpoConfig): Client
   isStarted(): boolean
