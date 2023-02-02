15121039d8157e4d989a19cdf4e0b7fc83445119
hannah-smartbear <hannah.syed@smartbear.com>
2023-02-02 14:08:25 +0000

    Update lib/insert.js to insert snippets into either app.ts or app.js

--- packages/expo-cli/lib/test/insert.test.js
+++ packages/expo-cli/lib/test/insert.test.js
@@ -3,13 +3,36 @@ const insert = require('../insert')
 const { readFile } = require('fs/promises')
 
 describe('expo-cli: insert', () => {
-  it('should work on a fresh project', async () => {
+  it('should work on a fresh javascript project', async () => {
     await withFixture('blank-00', async (projectRoot) => {
       const msg = await insert(projectRoot)
       expect(msg).toBe(undefined)
 
       const appJs = await readFile(`${projectRoot}/App.js`, 'utf8')
       expect(appJs).toMatch(/^import Bugsnag from '@bugsnag\/expo';\sBugsnag.start\(\);\s/)
+    })
+  })
+
+  it('should work on a fresh typescript project', async () => {
+    await withFixture('blank-01', async (projectRoot) => {
+      const msg = await insert(projectRoot)
+      expect(msg).toBe(undefined)
+
+      const appTs = await readFile(`${projectRoot}/App.ts`, 'utf8')
+      expect(appTs).toMatch(/^import Bugsnag from '@bugsnag\/expo';\sBugsnag.start\(\);\s/)
+    })
+  })
+
+  it('should prioritise .ts over .js', async () => {
+    await withFixture('blank-02', async (projectRoot) => {
+      const appJsBefore = await readFile(`${projectRoot}/App.js`, 'utf8')
+      const msg = await insert(projectRoot)
+      expect(msg).toBe(undefined)
+
+      const appTs = await readFile(`${projectRoot}/App.ts`, 'utf8')
+      const appJsAfter = await readFile(`${projectRoot}/App.js`, 'utf8')
+      expect(appTs).toMatch(/^import Bugsnag from '@bugsnag\/expo';\sBugsnag.start\(\);\s/)
+      expect(appJsAfter).toBe(appJsBefore)
     })
   })
 
@@ -35,9 +58,9 @@ describe('expo-cli: insert', () => {
     })
   })
 
-  it('should provide a reasonable error when there is no App.js', async () => {
+  it('should provide a reasonable error when there is no App.js or App.ts', async () => {
     await withFixture('empty-00', async (projectRoot) => {
-      await expect(insert(projectRoot)).rejects.toThrow(/^Couldn’t find App\.js in/)
+      await expect(insert(projectRoot)).rejects.toThrow(/^Couldn’t find App\.js or App\.ts in/)
     })
   })
 
