94ca81b73994801af290ef000e1acce8f4861274
hannah-smartbear <hannah.syed@smartbear.com>
2023-02-02 14:08:25 +0000

    Update lib/insert.js to insert snippets into either app.ts or app.js

--- packages/expo-cli/lib/insert.js
+++ packages/expo-cli/lib/insert.js
@@ -1,5 +1,5 @@
 const { join } = require('path')
-const { readFile, writeFile } = require('fs')
+const { readFile, writeFile, existsSync } = require('fs')
 const { promisify } = require('util')
 const { detectInstalledVersion } = require('./detect-installed')
 const semver = require('semver')
@@ -8,19 +8,32 @@ const importRe = /from ["']@bugsnag\/expo["']/
 const requireRe = /require\(["']@bugsnag\/expo["']\)/
 
 module.exports = async (projectRoot) => {
-  try {
-    const appJsPath = join(projectRoot, 'App.js')
-    const appJs = await promisify(readFile)(appJsPath, 'utf8')
-    if (importRe.test(appJs) || requireRe.test(appJs)) {
-      return '@bugsnag/expo is already imported in App.js'
+  //check if app.ts exists. If it does exist, check for/insert bugsnag in app.ts
+  const appTsPath = join(projectRoot, 'App.ts')
+  if (existsSync(appTsPath)) {
+    const appTs = await promisify(readFile)(appTsPath, 'utf8')
+    if (importRe.test(appTs) || requireRe.test(appTs)) {
+      return '@bugsnag/expo is already imported in App.ts'
     }
-    await promisify(writeFile)(appJsPath, `${await getCode(projectRoot)}\n${appJs}`, 'utf8')
-  } catch (e) {
-    // swallow and rethrow for errors that we can produce better messaging for
-    if (e.code === 'ENOENT') {
-      throw new Error(`Couldn’t find App.js in "${projectRoot}". Is this the root of your Expo project?`)
+    await promisify(writeFile)(appTsPath, `${await getCode(projectRoot)}\n${appTs}`, 'utf8')
+
+  //if app.ts doesn't exist, check for app.js and check for/insert bugsnag in app.js
+  } else {
+    try {
+      const appJsPath = join(projectRoot, 'App.js')
+      const appJs = await promisify(readFile)(appJsPath, 'utf8')
+      if (importRe.test(appJs) || requireRe.test(appJs)) {
+        return '@bugsnag/expo is already imported in App.js'
+      }
+      await promisify(writeFile)(appJsPath, `${await getCode(projectRoot)}\n${appJs}`, 'utf8')
     }
-    throw e
+    catch (e) {
+      // if app.js doesn't exist either, provide appropriate error messsage
+      if (e.code === 'ENOENT') {
+        throw new Error(`Couldn’t find App.js or App.ts in "${projectRoot}". Is this the root of your Expo project?`)
+      }
+      throw e
+    }
   }
 }
 
