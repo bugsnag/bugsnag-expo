60f1f9f56f14eb7c1864f3f0ee48506656fc8830
hannah-smartbear <hannah.syed@smartbear.com>
2023-02-06 16:05:01 +0000

    updating with suggested changes

--- packages/expo-cli/lib/test/insert.test.js
+++ packages/expo-cli/lib/test/insert.test.js
@@ -4,7 +4,7 @@ const { readFile } = require('fs/promises')
 
 describe('expo-cli: insert', () => {
   it('should work on a fresh javascript project', async () => {
-    await withFixture('blank-00', async (projectRoot) => {
+    await withFixture('blank-js', async (projectRoot) => {
       const msg = await insert(projectRoot)
       expect(msg).toBe(undefined)
 
@@ -13,18 +13,8 @@ describe('expo-cli: insert', () => {
     })
   })
 
-  it('should work on a fresh typescript project', async () => {
-    await withFixture('blank-01', async (projectRoot) => {
-      const msg = await insert(projectRoot)
-      expect(msg).toBe(undefined)
-
-      const appTs = await readFile(`${projectRoot}/App.ts`, 'utf8')
-      expect(appTs).toMatch(/^import Bugsnag from '@bugsnag\/expo';\sBugsnag.start\(\);\s/)
-    })
-  })
-
-  it('should prioritise .ts over .js', async () => {
-    await withFixture('blank-02', async (projectRoot) => {
+  it('should work on a fresh typescript project and prioritise .ts over .js', async () => {
+    await withFixture('blank-ts-and-js', async (projectRoot) => {
       const appJsBefore = await readFile(`${projectRoot}/App.js`, 'utf8')
       const msg = await insert(projectRoot)
       expect(msg).toBe(undefined)
@@ -36,25 +26,47 @@ describe('expo-cli: insert', () => {
     })
   })
 
-  it('shouldn’t insert if @bugsnag/expo is already imported (import)', async () => {
-    await withFixture('already-configured-00', async (projectRoot) => {
-      const appJsBefore = await readFile(`${projectRoot}/app.json`, 'utf8')
+  it('shouldn’t insert if @bugsnag/expo is already imported (import, js)', async () => {
+    await withFixture('already-configured-js-import', async (projectRoot) => {
+      const appJsBefore = await readFile(`${projectRoot}/App.js`, 'utf8')
       const msg = await insert(projectRoot)
       expect(msg).toMatch(/already/)
 
-      const appJsAfter = await readFile(`${projectRoot}/app.json`, 'utf8')
+      const appJsAfter = await readFile(`${projectRoot}/App.js`, 'utf8')
       expect(appJsAfter).toBe(appJsBefore)
     })
   })
 
-  it('shouldn’t insert if @bugsnag/expo is already imported (require)', async () => {
-    await withFixture('already-configured-00', async (projectRoot) => {
-      const appJsBefore = await readFile(`${projectRoot}/app.json`, 'utf8')
+  it('shouldn’t insert if @bugsnag/expo is already imported (require, js)', async () => {
+    await withFixture('already-configured-js-require', async (projectRoot) => {
+      const appJsBefore = await readFile(`${projectRoot}/App.js`, 'utf8')
       const msg = await insert(projectRoot)
       expect(msg).toMatch(/already/)
 
-      const appJsAfter = await readFile(`${projectRoot}/app.json`, 'utf8')
+      const appJsAfter = await readFile(`${projectRoot}/App.js`, 'utf8')
       expect(appJsAfter).toBe(appJsBefore)
+    })
+  })
+
+  it('shouldn’t insert if @bugsnag/expo is already imported (import, ts)', async () => {
+    await withFixture('already-configured-ts-import', async (projectRoot) => {
+      const appTsBefore = await readFile(`${projectRoot}/App.ts`, 'utf8')
+      const msg = await insert(projectRoot)
+      expect(msg).toMatch(/already/)
+
+      const appTsAfter = await readFile(`${projectRoot}/App.ts`, 'utf8')
+      expect(appTsAfter).toBe(appTsBefore)
+    })
+  })
+
+  it('shouldn’t insert if @bugsnag/expo is already imported (require, ts)', async () => {
+    await withFixture('already-configured-ts-require', async (projectRoot) => {
+      const appTsBefore = await readFile(`${projectRoot}/App.ts`, 'utf8')
+      const msg = await insert(projectRoot)
+      expect(msg).toMatch(/already/)
+
+      const appTsAfter = await readFile(`${projectRoot}/App.ts`, 'utf8')
+      expect(appTsAfter).toBe(appTsBefore)
     })
   })
 
@@ -68,8 +80,8 @@ describe('expo-cli: insert', () => {
     await expect(insert(/* projectRoot is required */)).rejects.toThrow(/The "path" argument must be of type string/)
   })
 
-  it('inserts correct code for pre v7 versions of Bugsnag', async () => {
-    await withFixture('already-installed-00', async (projectRoot) => {
+  it('inserts correct code for pre v7 versions of Bugsnag (js)', async () => {
+    await withFixture('already-installed-prev7-js', async (projectRoot) => {
       const msg = await insert(projectRoot)
       expect(msg).toBe(undefined)
 
@@ -78,8 +90,8 @@ describe('expo-cli: insert', () => {
     })
   })
 
-  it('inserts correct code for post v7.0.0 versions of Bugsnag', async () => {
-    await withFixture('already-installed-01', async (projectRoot) => {
+  it('inserts correct code for post v7.0.0 versions of Bugsnag (js)', async () => {
+    await withFixture('already-installed-postv7-js', async (projectRoot) => {
       const msg = await insert(projectRoot)
       expect(msg).toBe(undefined)
 
@@ -87,4 +99,25 @@ describe('expo-cli: insert', () => {
       expect(appJs).toMatch(/^import Bugsnag from '@bugsnag\/expo';\sBugsnag\.start\(\);\s/)
     })
   })
+
+  it('inserts correct code for pre v7 versions of Bugsnag (ts)', async () => {
+    await withFixture('already-installed-prev7-ts', async (projectRoot) => {
+      const msg = await insert(projectRoot)
+      expect(msg).toBe(undefined)
+
+      const appJs = await readFile(`${projectRoot}/App.ts`, 'utf8')
+      expect(appJs).toMatch(/^import bugsnag from '@bugsnag\/expo';\sconst bugsnagClient = bugsnag\(\);\s/)
+    })
+  })
+
+  it('inserts correct code for post v7.0.0 versions of Bugsnag (ts)', async () => {
+    await withFixture('already-installed-postv7-ts', async (projectRoot) => {
+      const msg = await insert(projectRoot)
+      expect(msg).toBe(undefined)
+
+      const appJs = await readFile(`${projectRoot}/App.ts`, 'utf8')
+      expect(appJs).toMatch(/^import Bugsnag from '@bugsnag\/expo';\sBugsnag\.start\(\);\s/)
+    })
+  })
+
 })
