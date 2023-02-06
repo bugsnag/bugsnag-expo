ccd8d2a8f31dfd41e90e9f1cf09dc3228632c238
hannah-smartbear <hannah.syed@smartbear.com>
2023-02-06 16:05:01 +0000

    updating with suggested changes

--- packages/expo-cli/lib/insert.js
+++ packages/expo-cli/lib/insert.js
@@ -8,31 +8,29 @@ const importRe = /from ["']@bugsnag\/expo["']/
 const requireRe = /require\(["']@bugsnag\/expo["']\)/
 
 module.exports = async (projectRoot) => {
-  // check if app.ts exists. If it does exist, check for/insert bugsnag in app.ts
-  const appTsPath = join(projectRoot, 'App.ts')
-  if (existsSync(appTsPath)) {
-    const appTs = await promisify(readFile)(appTsPath, 'utf8')
-    if (importRe.test(appTs) || requireRe.test(appTs)) {
-      return '@bugsnag/expo is already imported in App.ts'
-    }
-    await promisify(writeFile)(appTsPath, `${await getCode(projectRoot)}\n${appTs}`, 'utf8')
 
-  // if app.ts doesn't exist, check for app.js and check for/insert bugsnag in app.js
-  } else {
-    try {
-      const appJsPath = join(projectRoot, 'App.js')
-      const appJs = await promisify(readFile)(appJsPath, 'utf8')
-      if (importRe.test(appJs) || requireRe.test(appJs)) {
-        return '@bugsnag/expo is already imported in App.js'
-      }
-      await promisify(writeFile)(appJsPath, `${await getCode(projectRoot)}\n${appJs}`, 'utf8')
-    } catch (e) {
-      // if app.js doesn't exist either, provide appropriate error messsage
-      if (e.code === 'ENOENT') {
-        throw new Error(`Couldn’t find App.js or App.ts in "${projectRoot}". Is this the root of your Expo project?`)
-      }
-      throw e
+  function checkFileExists(filename) {
+    const appPath = join(projectRoot, filename)
+    return existsSync(appPath)
+  }
+
+  const writeBugsnagImport = async (filename) => {
+    // check if import statement has already been added and return
+    const appPath = join(projectRoot, filename)
+    const app = await promisify(readFile)(appPath, 'utf8')
+    if (importRe.test(app) || requireRe.test(app)) {
+      return `@bugsnag/expo is already imported in ${filename}`
     }
+   // write to file
+    await promisify(writeFile)(appPath, `${await getCode(projectRoot)}\n${app}`, 'utf8')
+  }
+
+  if (checkFileExists('App.ts')) {
+    return await writeBugsnagImport('App.ts')
+  } else if (checkFileExists('App.js')) {
+    return await writeBugsnagImport('App.js')
+  } else {
+    throw new Error(`Couldn’t find App.js or App.ts in "${projectRoot}". Is this the root of your Expo project?`)
   }
 }
 
