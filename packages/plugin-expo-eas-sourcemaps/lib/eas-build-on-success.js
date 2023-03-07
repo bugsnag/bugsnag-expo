cf7093e96371a009e066bec583c1ced2c2a3fd76
Yousif Ahmed <yousif@bugsnag.com>
2023-03-07 13:11:40 +0000

    use promises instead of callbacks

--- packages/plugin-expo-eas-sourcemaps/lib/eas-build-on-success.js
+++ packages/plugin-expo-eas-sourcemaps/lib/eas-build-on-success.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-const { access, constants } = require('fs')
+const { access } = require('fs').promises
 const { reactNative } = require('@bugsnag/source-maps')
 const { exit } = require('process')
+
+const PROJECT_ROOT = process.cwd()
 
 if (process.env.EAS_BUILD_PLATFORM !== 'android') {
   console.log('Skipping Android source map upload: iOS build detected')
@@ -12,39 +14,35 @@ if (process.env.EAS_BUILD_PLATFORM !== 'android') {
   exit(0)
 }
 
-const bundle = `${process.env.EAS_BUILD_WORKINGDIR}/android/app/build/generated/assets/createBundleReleaseJsAndAssets/index.android.bundle`
-access(bundle, constants.R_OK, error => {
-  if (error) {
-    console.log(`Skipping Android source map upload: App bundle ${bundle} could not be found.`)
+const uploadSourceMaps = async () => {
+  const bundle = `${PROJECT_ROOT}/android/app/build/generated/assets/createBundleReleaseJsAndAssets/index.android.bundle`
+  await access(bundle).catch((error) => {
+    console.log(`Skipping Android source map upload: App bundle ${bundle} could not be found.\n${error}`)
     exit(0)
-  }
-})
+  })
 
-const sourceMap = `${process.env.EAS_BUILD_WORKINGDIR}/android/app/build/generated/sourcemaps/react/release/index.android.bundle.map`
-access(sourceMap, constants.R_OK, error => {
-  if (error) {
+  const sourceMap = `${PROJECT_ROOT}/android/app/build/generated/sourcemaps/react/release/index.android.bundle.map`
+  await access(sourceMap).catch((error) => {
     console.error(`Error: source map ${sourceMap} could not be found.\n${error}`)
     exit(1)
+  })
+
+  let appConfig, apiKey
+  try {
+    appConfig = require(`${PROJECT_ROOT}/app.json`)
+    apiKey = appConfig?.expo?.extra?.bugsnag?.apiKey
+  } catch (error) {
+    console.error(`Error: Failed to load app.json file ${PROJECT_ROOT}/app.json.\n${error}`)
+    exit(1)
   }
-})
 
-let appConfig, apiKey
-try {
-  appConfig = require(`${process.cwd()}/app.json`)
-  apiKey = appConfig?.expo?.extra?.bugsnag?.apiKey
-} catch (error) {
-  console.error(`Error: Failed to load app.json file ${process.cwd()}/app.json.\n${error}`)
-  exit(1)
-}
+  if (!apiKey) {
+    console.error('Error: No Bugsnag API key detected in app.json')
+    exit(1)
+  }
 
-if (!apiKey) {
-  console.error('Error: No Bugsnag API key detected in app.json')
-  exit(1)
-}
-
-const uploadSourceMaps = async () => {
   console.log('Uploading Android source map to Bugsnag...')
-  return await reactNative.uploadOne({
+  await reactNative.uploadOne({
     apiKey,
     bundle,
     sourceMap,
