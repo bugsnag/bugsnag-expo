a07d3048d12be22587bbf387d037ed28730901d5
Yousif Ahmed <yousif@bugsnag.com>
2023-07-10 10:40:47 +0100

    bump latest supported version in CLI

--- packages/expo-cli/lib/version-information.js
+++ packages/expo-cli/lib/version-information.js
@@ -1,7 +1,7 @@
 const semver = require('semver')
 
 // the major version number of the latest Expo SDK we support
-const LATEST_SUPPORTED_EXPO_SDK = 48
+const LATEST_SUPPORTED_EXPO_SDK = 49
 
 class Version {
   constructor (expoSdkVersion, bugsnagVersion, isLegacy = false) {
