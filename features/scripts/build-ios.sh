5b9c2cda27aa67adb0df5c08d06a96c4f204f043
Yousif Ahmed <yousif@bugsnag.com>
2024-05-15 13:59:31 +0100

    test: set a custom working directory for ios eas builds

--- features/scripts/build-ios.sh
+++ features/scripts/build-ios.sh
@@ -2,6 +2,12 @@ set -e
 
 ./features/scripts/build-common.sh
 
+# specifying a working directory for the local eas build helps to avoid caching issues with metro
+if [[ -z ${EAS_LOCAL_BUILD_WORKINGDIR:-} ]]; then
+  EAS_LOCAL_BUILD_WORKINGDIR=$BUILDKITE_BUILD_CHECKOUT_PATH/features/fixtures/build
+  export EAS_LOCAL_BUILD_WORKINGDIR
+fi
+
 pushd features/fixtures/test-app
 
 eas build \
