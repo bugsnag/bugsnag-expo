a14a5f3b28c652cfb2257d8513a7ade646297686
Yousif Ahmed <yousif@bugsnag.com>
2025-09-11 13:50:41 +0100

    chore: update metro config based on expo doctor

--- features/fixtures/test-app/metro.config.js
+++ features/fixtures/test-app/metro.config.js
@@ -9,7 +9,7 @@ const workspaceRoot = path.resolve(projectRoot, '../../..');
 const config = getDefaultConfig(projectRoot);
 
 // 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
+config.watchFolders.push(workspaceRoot);
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
